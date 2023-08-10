5ef203b3b7cefedb4f489ab56430754a963a638b
Felipe Goncalves <felipeepires.95@gmail.com>
2023-08-09 21:41:48 -0300

    adicionando tela de menu principal

--- app/src/main/java/com/example/engetech/Photo.kt
+++ app/src/main/java/com/example/engetech/Photo.kt
@@ -12,6 +12,11 @@ class Photo : AppCompatActivity() {
     private lateinit var button: Button
     private lateinit var imageView: ImageView
 
+    private lateinit var button2: Button
+    private lateinit var imageView2: ImageView
+
+    var verificaCondicao: Boolean = false
+
     companion object{
         val IMAGE_REQUEST_CODE = 100
     }
@@ -23,8 +28,19 @@ class Photo : AppCompatActivity() {
         button = findViewById(R.id.btn_foto)
         imageView = findViewById(R.id.img_view_foto1)
 
+        button2 = findViewById(R.id.btn_foto2)
+        imageView2 = findViewById(R.id.img_view_foto2)
+
         button.setOnClickListener{
             pegarImagemGaleria()
+        }
+        button2.setOnClickListener{
+            pegarImagemGaleria2()
+        }
+
+        val buttonSalvar : Button = findViewById(R.id.btn_salvar_photo)
+        buttonSalvar.setOnClickListener{
+            tela_menu_principal()
         }
 
     }
@@ -34,17 +50,30 @@ class Photo : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, IMAGE_REQUEST_CODE)
+        verificaCondicao = true
+    }
+    private fun pegarImagemGaleria2(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+        verificaCondicao = false
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK && verificaCondicao){
             imageView.setImageURI(data?.data)
         }
+        else
+            imageView2.setImageURI(data?.data)
+
     }
 
-
-
+    //Funcao pra chamar tela de menu principal
+    private fun tela_menu_principal(){
+        val menu_principal = Intent(this,MenuPrincipal::class.java)
+        startActivity(menu_principal)
+    }
 
 
 }
